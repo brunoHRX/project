d76e392a734530cf2e0588b1985fa8ea82fe90df
brunoHRX <bruno.hrx@gmail.com>
2022-12-03 22:53:14 -0400

    Code IMP

--- SalonApp/src/screens/SignIn/index.js
+++ SalonApp/src/screens/SignIn/index.js
@@ -1,5 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {useNavigation} from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import {UserContext} from '../../contexts/UserContext';
+
 import {
   Container,
   InputArea,
@@ -10,6 +14,8 @@ import {
   SignMessageButtonTextBold,
 } from './styles';
 
+import Api from '../../Api';
+
 import SignInput from '../../components/SignInput';
 
 import BarberLogo from '../../assets/barber.svg';
@@ -17,12 +23,35 @@ import EmailIcon from '../../assets/email.svg';
 import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+  const {dispatch: userDispatch} = useContext(UserContext);
   const navigation = useNavigation();
 
   const [emailField, setEmailField] = useState('');
   const [passwordField, setPasswordField] = useState('');
 
-  const handleSignClick = () => {};
+  const handleSignClick = async () => {
+    if (emailField != '' && passwordField != '') {
+      let json = await Api.signIn(emailField, passwordField);
+      if (json.token) {
+        await AsyncStorage.setItem('token', json.token);
+
+        userDispatch({
+          type: 'setAvatar',
+          playload: {
+            avatar: json.data.avatar,
+          },
+        });
+
+        navigation.reset({
+          routes: [{name: 'MainTab'}],
+        });
+      } else {
+        alert('Email e/ou senha errados!');
+      }
+    } else {
+      alert('Preencha os Campos');
+    }
+  };
   const handleMessageButtonClick = () => {
     navigation.reset({
       routes: [{name: 'SignUp'}],
